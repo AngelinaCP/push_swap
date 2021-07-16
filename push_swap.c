565e6045a0237539909876126baf88b8a2080a74
Ангелина Банникова <angelinabannikova@Angelinas-MacBook-Air.local>
2021-07-16 13:38:22 +0300

    first

---
+++ push_swap.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include "../libft/libft.h"
+
+int check_argv(char **argv)
+{
+    int     i;
+    char    *str;
+
+    i = 1;
+    while (argv[i])
+    {
+        // if (!ft_atoi(argv[i]))
+        //     printf("ERROR\n");
+        if (!ft_isdigit(argv[i][0]))
+        {
+            printf("ERROR\n");
+            return (1);
+        }
+            
+        i++;
+    }
+    int j;
+
+    i = 1;
+    while (argv[i])
+    {
+        j = i + 1;
+        while (argv[j])
+        {
+            if (!ft_strncmp(argv[i], argv[j], ft_strlen(argv[1])))
+            {
+                printf("ERROR\n");
+                return (2);
+            }
+                
+            j++;
+        }
+        i++;
+    }
+    return (0);
+}
+
+void    ft_swap(int *a, int *b)
+{
+    int c;
+
+    c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void    rra(int *array)
+{
+    int c;
+
+    c = array[0];
+    array[0] = array[2];
+    array[2] = array[1];
+    array[1] = c;
+}
+
+void    ra(int *array)
+{
+    int c;
+
+    c = array[2];
+    array[2] = array[0];
+    array[0] = array[1];
+    array[1] = c;
+}
+
+void    algorithm_3(int argc, int *stackA)
+{
+    if (stackA[0] > stackA[1] && stackA[0] < stackA[2])
+        ft_swap(&stackA[0], &stackA[1]);
+    else if (stackA[0] > stackA[1] && stackA[1] > stackA[2])
+    {
+        ft_swap(&stackA[0], &stackA[1]);
+        rra(stackA);
+    }
+    else if (stackA[0] > stackA[1] && stackA[1] < stackA[2])
+        ra(stackA);
+    else if (stackA[0] < stackA[1] && stackA[0] < stackA[2])
+    {
+        ft_swap(&stackA[0], &stackA[1]);
+        ra(stackA);
+    }
+    else if (stackA[0] < stackA[1] && stackA[0] < stackA[1])
+        rra(stackA);
+  //  return (0);
+}
+
+void    remove_elements(int *array, int argc, int num)
+{
+    int i;
+
+    i = 0;
+    while (i < argc - 1)
+    {
+        array[i] = array[i + num];
+        i++;
+    }
+    
+}
+
+void pa(int *stackA, int *stackB, int argc)
+{
+    int i;
+    int size;
+
+    i = 0;
+    size = sizeof(&stackA) / sizeof(int);
+    size++;
+    while (size)
+    {
+        stackA[size] = stackA[size - 1];
+        size--;
+    }
+    stackA[0] = stackB[0];
+    size = sizeof(&stackA) / sizeof(int);
+    size++;
+    i = 0;
+    while (i > size)
+    {
+        stackB[i] = stackB[i + 1];
+        i++;
+    }
+}
+
+void pb(int *stackA, int *stackB)
+{
+    int size;
+
+    size = sizeof(&stackB) / sizeof(int);
+    if (!stackB)
+      stackB[0] = stackA[0];
+    else
+    {
+         size++;
+        while (size)
+        {
+            stackB[size] = stackB[size - 1];
+            size--;
+        }
+        stackB[0] = stackA[0]; 
+    }
+}
+
+int algorithm_5(int argc, int *stackA, int *stackB)
+{
+    int i;
+
+    i = 0;
+    pb(stackA, stackB);
+    remove_elements(stackA, argc, 2);
+    algorithm_3(argc, stackA);
+    pa(stackA, stackB, argc);
+    while (stackA[i] > stackA[i + 1])
+        ft_swap(&stackA[i], &stackA[i + 1]);
+    pa(stackA, stackB, argc);
+    // while (stackA[i] > stackA[i + 1])
+    //     ft_swap(&stackA[i], &stackA[i + 1]);
+      for (int i = 0; i < 5; i++)
+        printf("%d\n", stackA[i]);
+    return (0);
+}
+
+void check_num(int argc, int *stackA, int *stackB)
+{
+    if (argc == 4)
+        algorithm_3(argc, stackA);
+    else if (argc == 6)
+        algorithm_5(argc, stackA, stackB);
+
+}
+
+int check_if_sorted(int argc, int *stackA)
+{
+    int i;
+
+    i = 0;
+    while (i + 1 < argc - 1)
+    {
+        if (stackA[i] > stackA[i + 1])
+            return (0);
+        i++;
+    }
+    printf("Sorted\n");
+    return (1);
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    int *stackA;
+    int *stackB;
+
+    i = 1;
+    stackA = (int *)malloc(sizeof(int) * argc - 1);
+    stackB = (int *)malloc(sizeof(int) * argc - 1);
+    while (i < argc)
+    {
+        stackA[i - 1] = ft_atoi(argv[i]);
+        i++;
+    }
+    if (check_if_sorted(argc, stackA))
+        return (0);
+    if (check_argv(argv))
+        return (0);
+    check_num(argc, stackA, stackB);
+}
---
+++ test.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Node
+{
+	int	x;
+	struct Node *next;
+} Node;
+
+void insert_end(Node **root, int value)
+{
+	Node *new_node = malloc(sizeof(Node));
+	if (!new_node)
+		exit (1);
+	new_node->next = NULL;
+	new_node->x = value;
+	if (*root == NULL)
+	{
+		*root = new_node;
+		return ;
+	}
+	Node *curr = *root;
+
+	while (curr->next)
+		curr = curr->next;
+	curr->next = new_node;
+}
+
+void insert_beggining(Node **root, int value)
+{
+	Node	*new_node = malloc(sizeof(Node));
+	if (!new_node)
+		exit (1);
+	new_node->x = value;
+	new_node->next = *root;
+	
+	*root = new_node;
+}
+
+void	insert_after(Node *node, int value)
+{
+	Node	*new_node = malloc(sizeof(Node));
+
+	if (new_node == NULL)
+		exit(2);
+	new_node->x = value;
+	new_node->next = node->next;
+	node->next = new_node;
+}
+
+void	insert_sorted(Node **root, int value)
+{
+	if (*root == NULL || (*root)->x >= value)
+	{
+		insert_beggining(root, value);
+		return ;
+	}
+	Node	*curr;
+	
+	curr = *root;
+	while (curr->next != NULL)
+	{
+		if (curr->next->x >= value)
+			break;
+		curr = curr->next;
+	}
+	insert_after(curr, value);
+}
+
+void	remove_element(Node **root, int value)
+{
+	if (*root == NULL)
+		return ;
+	Node	*curr = *root;
+	Node	*to_remove = NULL;
+	
+	if ((*root)->x == value)
+	{
+		to_remove = *root;
+		*root = (*root)->next;
+		free (to_remove);
+		return ;
+	}
+	while (curr->next != NULL)
+	{
+		if (curr->next->x == value)
+		{
+			to_remove = curr->next;
+			curr->next = curr->next->next;
+			free (to_remove);
+		}
+		curr = curr->next;
+	}
+}
+
+void deallocate(Node **root)
+{
+	Node	*curr = *root;
+	while (curr)
+	{
+		Node *aux = curr;
+		curr = curr->next;
+		free (aux);
+	}
+	*root = NULL;
+}
+
+void reverse(Node **root)
+{
+	Node *prev = NULL;
+	Node *curr = *root;
+}
+
+int main(void)
+{
+	Node *root = NULL;
+	// Node *root = malloc(sizeof(Node));
+	// if (!root)
+	// 	exit (1);
+	// root->x = 5;
+	// root->next = NULL;
+	//insert_beggining(&root, 2);
+	// insert_after(root, 12);
+	// insert_end(&root, 10);
+	insert_sorted(&root, 5);
+	insert_sorted(&root, 4);
+	insert_sorted(&root, 1);
+	insert_sorted(&root, 7);
+	remove_element(&root, 1);
+	Node	*curr = root;
+
+	while (curr != NULL)
+	{
+		printf("%d\n", curr->x);
+		curr = curr->next;
+	}
+
+	return (0);
+} 
